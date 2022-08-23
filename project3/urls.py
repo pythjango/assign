31e2fb3ed4a9603d75c4b6a0bcaae78478911a89
pythjango <malutheertha481@gmail.com>
2022-08-23 09:58:41 +0530

    add text

--- project3/urls.py
+++ project3/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('seller/',include(seller.urls'))
+    path ('seller/',include('seller.urls'))
 ]
